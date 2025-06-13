21e49f2b01bde3560c3e459a8301078e6a953473
Jafar777 <jafaralsoubeni@gmail.com>
2025-06-13 12:17:23 -0700

    dist

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'public')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'public', 'dist', 'index.html'));
   });
 }
 
